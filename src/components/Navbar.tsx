f9bfce4ad2a4beadec0a5cd63dcb60b5255c2c2a
Vrushabh <vrushabhgawas14@gmail.com>
2024-10-20 23:55:57 +0530

    Navbar Updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 export default function Navbar() {
-    return (
-        <>
-            <div>I am Navbar</div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex p-2 m-4 space-x-4 text-2xl">
+        <div>Home</div>
+        <div>About</div>
+        <div>Tour</div>
+        <div>Offers</div>
+        <div>Explore</div>
+      </div>
+    </>
+  );
 }
