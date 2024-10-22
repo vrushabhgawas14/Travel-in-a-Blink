cca06e833d9b9ed171c1dc73093cc5876270f68c
Vrushabh <vrushabhgawas14@gmail.com>
2024-10-22 11:17:19 +0530

    Footer Updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <>
-      <div>I am Footer</div>
+      <div className="text-center pt-4 pb-2 text-sm text-gray-800 dark:text-gray-800 absolute bottom-0 right-1/2 translate-x-1/2">
+        <p>© 2024 Developed by Vrushabh Gawas.</p>
+      </div>
     </>
   );
 }
