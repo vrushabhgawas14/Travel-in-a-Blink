9bd07dfcd03803b9035a908cc520a2bbe4e93c03
Vrushabh <vrushabhgawas14@gmail.com>
2024-10-23 23:47:02 +0530

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,21 @@ export default function Navbar() {
   return (
     <>
       <div className="flex p-2 m-4 space-x-4 text-2xl">
-        <div>Home</div>
-        <div>About</div>
-        <div>Tour</div>
-        <div>Offers</div>
-        <div>Explore</div>
+        <div>
+          <a href="/">Home</a>
+        </div>
+        <div>
+          <a href="/about">About</a>
+        </div>
+        <div>
+          <a href="/tour">Tour</a>
+        </div>
+        <div>
+          <a href="/offers">Offers</a>
+        </div>
+        <div>
+          <a href="/explore">Explore</a>
+        </div>
       </div>
     </>
   );
