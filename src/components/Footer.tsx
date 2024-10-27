6f02c8b29d0ff6aaf869c0071c81f5c1a2c4c7b0
Vrushabh <vrushabhgawas14@gmail.com>
2024-10-27 17:18:48 +0530

    Footer full width Updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <div className="text-center pt-4 pb-2 text-sm text-gray-800 dark:text-gray-800 absolute bottom-0 right-1/2 translate-x-1/2">
+      <div className="text-center pt-4 pb-2 w-full text-sm text-gray-800 absolute bottom-0 right-1/2 translate-x-1/2">
         <p>© 2024 Developed by Vrushabh Gawas.</p>
       </div>
     </>
