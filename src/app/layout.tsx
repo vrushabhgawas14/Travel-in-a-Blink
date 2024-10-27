bbeb068e8bb340efbe3b1cfd21b68ac2731d3720
Vrushabh <vrushabhgawas14@gmail.com>
2024-10-27 08:44:26 +0530

    Responsiveness of Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-purple-200">
         <Navbar />
         {children}
         <Footer />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,32 @@
-import { NavFirstHalf, NavSecondHalf } from "@/constants/NavbarDetails";
+import {
+  hamburgerMenu,
+  NavFirstHalf,
+  NavSecondHalf,
+} from "@/constants/NavbarDetails";
 
 export default function Navbar() {
   return (
     <>
-      <div className="flex items-center w-full text-2xl font-semibold py-10">
-        <div className="flex justify-evenly w-[38%] py-2">
+      <div className="flex items-center justify-between w-full text-2xl font-semibold py-10 sm:p-8 sm:w-auto">
+        <div className="flex justify-evenly w-[38%] py-2 sm:hidden">
           {NavFirstHalf.map((item) => (
-            <a key={item.id} href={item.url} target="_blank">
+            <a key={item.id} href={item.url}>
               {item.text}
             </a>
           ))}
         </div>
-        <div className="text-center font-bold text-4xl w-[24%]">
+        <div className="text-center font-bold text-4xl w-[24%] sm:text-3xl sm:w-auto">
           <a href="/">Travel in a Blink</a>
         </div>
-        <div className="flex justify-evenly w-[38%] py-2">
+        <div className="flex justify-evenly w-[38%] py-2 sm:hidden">
           {NavSecondHalf.map((item) => (
-            <a key={item.id} href={item.url} target="_blank">
+            <a key={item.id} href={item.url}>
               {item.text}
             </a>
           ))}
+        </div>
+        <div className="hidden sm:block sm:w-auto">
+          <button className="w-6 h-6">{hamburgerMenu.bar}</button>
         </div>
       </div>
     </>
