26d112a1ac6cbb4fb4f9d2158e55b6ddc64d42e4
Vrushabh <vrushabhgawas14@gmail.com>
2024-10-27 09:13:47 +0530

    Toggle menu of Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,28 @@
+"use client";
+
 import {
   hamburgerMenu,
   NavFirstHalf,
   NavSecondHalf,
 } from "@/constants/NavbarDetails";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  let hamburger = hamburgerMenu.bar;
+
+  if (isOpen) {
+    hamburger = hamburgerMenu.cross;
+  } else {
+    hamburger = hamburgerMenu.bar;
+  }
+  const dropDown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <div className="flex items-center justify-between w-full text-2xl font-semibold py-10 sm:p-8 sm:w-auto">
+      <div className="flex items-center justify-between w-full text-2xl font-semibold py-10 sm:p-6 sm:w-auto">
         <div className="flex justify-evenly w-[38%] py-2 sm:hidden">
           {NavFirstHalf.map((item) => (
             <a key={item.id} href={item.url}>
@@ -26,7 +41,9 @@ export default function Navbar() {
           ))}
         </div>
         <div className="hidden sm:block sm:w-auto">
-          <button className="w-6 h-6">{hamburgerMenu.bar}</button>
+          <button className="w-6 h-6" onClick={dropDown}>
+            {hamburger}
+          </button>
         </div>
       </div>
     </>
