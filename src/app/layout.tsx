19d039907e7438e9e90d9a78fff8321c17c2f604
Vrushabh <vrushabhgawas14@gmail.com>
2024-11-01 15:33:56 +0530

    Body text black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body className="bg-black text-black">
         <Navbar />
         {children}
         <Footer />
